e3aa199fc2c7a419d6819c9bce6d5287f56f7f19
iamansh77 <116120146+iamansh77@users.noreply.github.com>
2022-10-19 03:36:05 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class config:
     SUDO_USERS = "850731548" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "937809899637-63ukm7q277odg017kgep6fcrugsv6gvm.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "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"
 
 
 
